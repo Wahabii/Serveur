7be70045484df9bca30307351c43f3738696d962
Baha Ferchichi <ferchichibaha7@gmail.com>
2020-12-28 20:12:10 +0100

    cors module + minor cleanup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require('cors')
 
 const app = express();
 var bodyParser = require("body-parser");
@@ -11,19 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Setup CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-  });
-
+  app.use(cors());
 // Setup static files path
 app.use("/uploads", express.static("uploads"));
 
